2ed585e04c9a605de04b40cab876fe0d49d06060
ajaypk207-tech <ajaypk207@gmail.com>
2026-02-08 23:20:18 +0530

    Add files via upload

---
+++ evenodd.js
@@ -0,0 +1,9 @@
+function check() {
+    let number = document.getElementById("num").value;
+
+    if (number % 2 == 0) {
+        document.getElementById("result").innerHTML = "Even Number";
+    } else {
+        document.getElementById("result").innerHTML = "Odd Number";
+    }
+}
